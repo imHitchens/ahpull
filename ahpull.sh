4adb2c53b580162ed985a8b9c9641145c6b6923b
imHitchens <git@skoog.dev>
2024-08-19 13:02:37 +0200

    Output format csv, separate /auctions and /tmp folders

--- ahpull.sh
+++ ahpull.sh
@@ -18,13 +18,14 @@ do
                 jq '.auctions[]' | jq '.id' | 
                 while read ahid;
                 do
-                    mkdir -p data/$namespace$region/tmp
+                    mkdir -p data/tmp
+                    mkdir -p mkdir -p data/auctions
                     write_date=$(date +%s)
                     curl --header "Authorization: Bearer $access_token" "https://$region.api.blizzard.com/data/wow/connected-realm/$connectedRealmId/auctions/$ahid?namespace=$namespace$region&$locale&$access_token" | 
                     jq ".auctions[]" | 
                     jq "{ region: \"$namespace$region\", connectedRealmId: $connectedRealmId, ahid: $ahid, id: .id, time: $write_date, itemid: .item.id, itemrand: .item.rand, itemseed: .item.seed, time_left: .time_left, bid: .bid, buyout: .buyout, quantity: .quantity }" | 
-                    jq -r "join(\";\")" >> data/$namespace$region/tmp/$connectedRealmId_$time.json
-                    mv data/$namespace$region/tmp/$connectedRealmId_$time.json data/$namespace$region/$connectedRealmId_$time.json ;
+                    jq -r "join(\";\")" >> data/tmp/$namespace$region$connectedRealmId_$time.csv
+                    mv data/tmp/$namespace$region$connectedRealmId_$time.csv data/auctions/$namespace$region$connectedRealmId_$time.csv ;
                 done;
             done;
         done;
